ccf9b0b1c3766a85fb51d2205823f0a3af090e20
UBCSamSung <anan1411@gmail.com>
2019-01-26 16:15:12 -0800

    push create-react-app stuff

---
+++ webapp/src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Camera from 'react-camera';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  takePicture() {
+    this.camera.capture()
+      .then(blob => {
+        this.img.src = URL.createObjectURL(blob);
+        this.img.onload = () => { URL.revokeObjectURL(this.src); }
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Camera
+            style={style.preview}
+            ref={(cam) => {
+              this.camera = cam;
+            }}
+          >
+            <div onClick={this.takePicture}>
+              <div style={style.captureButton} />
+            </div>
+          </Camera>
+        </header>
+      </div>
+    );
+  }
+}
+
+const style = {
+  preview: {
+    position: 'relative',
+  },
+  captureContainer: {
+    display: 'flex',
+    position: 'absolute',
+    justifyContent: 'center',
+    zIndex: 1,
+    bottom: 0,
+    width: '100%'
+  },
+  captureButton: {
+    backgroundColor: '#fff',
+    borderRadius: '50%',
+    height: 56,
+    width: 56,
+    color: '#000',
+    margin: 20
+  },
+  captureImage: {
+    width: '100%',
+  }
+};
+
+export default App;
