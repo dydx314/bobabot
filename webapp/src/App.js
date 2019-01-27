c7fc4a73dfaf9ca92eacd2abf5f979a3766e79ae
gwenli <guanyan1995@gmail.com>
2019-01-26 17:36:50 -0800

    fixing router and adding home image

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import FaceDetection from './components/camera.js'
+import { BrowserRouter as Router, Route,Redirect, Link } from "react-router-dom";
+import CameraPage from './components/camera.js'
+import home from './components/home.js'
 
 function App() {
   return (
     <Router>
       <div>
-        <ul>
-          <li>
-            <Link to="/faceRecoginition">FaceDetection</Link>
-          </li>
-        </ul>
+        
 
         <hr />
-        <Route path="/faceRecoginition" component={FaceDetection} />
+        <Route path="/" render={ () => <Redirect to="/home" />}/>
+        <Route path="/home" render={ () => <home/> } />
+        <Route path="/CameraPage" render={ () => <Camera/>} />
       </div>
     </Router>
   );
---
+++ webapp/src/components/home.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import StarryNight from './star_night_sky.jpg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import history from '../history.js'
+class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+
+    }
+
+    render() {
+        return (
+          <div>
+          <img src={StarryNight} height="1080" width="1920" className='fill'/>
+          <div style={style.Headings}>
+          <ul>
+          <p>
+          <a onClick={()=>{let path='/CameraPage'; 
+                          history.push(path);
+                        }}>Facial Detection</a>
+          </p>
+          </ul>
+          </div>
+            </div>
+        );
+    };
+}
+const style = {
+    Headings: {
+        position: 'absolute',
+        top: '130px',
+        left: '36%',
+        right: '50%',
+        width: 'fit-content'
+      }
+  };
+export default Home;
