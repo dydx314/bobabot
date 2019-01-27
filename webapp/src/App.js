88dafaf102fa311f62e4ffebf83d9c82f331e18e
UBCSamSung <anan1411@gmail.com>
2019-01-26 16:46:56 -0800

    added react router

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,77 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Camera from 'react-camera';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import FaceDetection from './components/camera.js'
 
-class App extends Component {
+function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/faceRecoginition">FaceDetection</Link>
+          </li>
+        </ul>
 
-  constructor(props) {
-    super(props);
-  }
-
-  takePicture() {
-    this.camera.capture()
-      .then(blob => {
-        this.img.src = URL.createObjectURL(blob);
-        this.img.onload = () => { URL.revokeObjectURL(this.src); }
-      })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Camera
-            style={style.preview}
-            ref={(cam) => {
-              this.camera = cam;
-            }}
-          >
-            <div onClick={this.takePicture}>
-              <div style={style.captureButton} />
-            </div>
-          </Camera>
-        </header>
+        <hr />
+        <Route path="/faceRecoginition" component={FaceDetection} />
       </div>
-    );
-  }
+    </Router>
+  );
 }
-
-const style = {
-  preview: {
-    position: 'relative',
-  },
-  captureContainer: {
-    display: 'flex',
-    position: 'absolute',
-    justifyContent: 'center',
-    zIndex: 1,
-    bottom: 0,
-    width: '100%'
-  },
-  captureButton: {
-    backgroundColor: '#fff',
-    borderRadius: '50%',
-    height: 56,
-    width: 56,
-    color: '#000',
-    margin: 20
-  },
-  captureImage: {
-    width: '100%',
-  }
-};
-
 export default App;
